6493761fc147eb60717ae63461dc507589d97856
Bintang1213 <140826237+Bintang1213@users.noreply.github.com>
2026-02-01 14:20:27 +0700

    validasi jk

--- dto/patient_request.go
+++ dto/patient_request.go
@@ -1,12 +1,12 @@
 package dto
 
 type CreatePatientRequest struct {
-	NIK          string  `json:"nik" binding:"required,len=16"`
-	NamaPasien   string  `json:"nama_pasien" binding:"required"`
-	JenisKelamin string  `json:"jenis_kelamin" binding:"required,oneof=L P"`
-	TanggalLahir string  `json:"tanggal_lahir" binding:"required"` 
-	TipePasien   string  `json:"tipe_pasien" binding:"required,oneof=baru lama"`
-	CaraBayar    string  `json:"cara_bayar" binding:"required,oneof=umum asuransi"`
-	NomorJaminan *string `json:"nomor_jaminan"`
-	PoliID       uint    `json:"poli_id" binding:"required"`
+    NIK           string  `json:"nik" binding:"required,len=16"`
+    NamaPasien    string  `json:"nama_pasien" binding:"required"`
+    JenisKelamin  string  `json:"jenis_kelamin" binding:"required,oneof=Laki-laki Perempuan"`
+    TanggalLahir  string  `json:"tanggal_lahir" binding:"required"` 
+    TipePasien    string  `json:"tipe_pasien" binding:"required,oneof=baru lama"`
+    CaraBayar     string  `json:"cara_bayar" binding:"required,oneof=umum asuransi"`
+    NomorJaminan  *string `json:"nomor_jaminan"`
+    PoliID        uint    `json:"poli_id" binding:"required"`
 }
